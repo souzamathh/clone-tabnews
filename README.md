0be5ad88a9790cec43a7c6591d99c2847ebeeb39
Matheus Souza <callmatheussouza@gmail.com>
2025-09-26 01:17:42 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # clone-tabnews
+
 Um projeto de estudo de programação do https://curso.dev para implementação do https://tabnews.com.br
 
 -
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Olá, mundo, como está?</h1>
+  return <h1>Olá, mundo, como está?</h1>;
+}
+
+function teste() {}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home; //define essa função de retorno como a principal para retornar o website
