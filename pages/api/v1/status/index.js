3338a8f2adb824bd16898739dd1f3262df1d3c92
Matheus Souza <callmatheussouza@gmail.com>
2026-02-15 18:15:35 +0000

    Endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,33 @@
-function status(request, response) {
-  //request (o que você quer receber de fora), response (o que você responder para fora)
-  response.status(200).json({ chave: "teste" }); //200 representa que está tudo OK
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version();");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'",
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  }); //200 representa que está tudo OK
 }
 
 export default status;
